1483b8c24b03345a2509745b72216d0a5b822a8f
SenpaiAZL <aryyaagata19@gmail.com>
2025-05-15 07:41:10 +0700

    fix electron funny ah

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require("electron/main");
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadURL("http://127.0.0.1:8000");
+  //   win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
